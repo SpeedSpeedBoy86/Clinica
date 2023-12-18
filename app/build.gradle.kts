8a6dc115b35e19233a007e07224448d7260887ae
Sander <sanderescobar12@gmail.com>
2023-12-17 22:32:08 -0300

    integre firebase a el proyecto y hice PushService.java con el codigo para las notificaciones pero nu lo pude testear bien porque no me funciona la emulacion virtual, tambien en el manifest le di permisos de notificaciones y de internet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -34,8 +35,10 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.hbb20:ccp:2.5.0")
+    implementation("com.google.firebase:firebase-messaging:23.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
 
 }
---
+++ app/src/main/java/com/example/clinica_caduceo/PushService.java
@@ -0,0 +1,135 @@
+package com.example.clinica_caduceo;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.clinica_caduceo.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class PushService extends FirebaseMessagingService {
+
+    private static final String TAG = "PushService";
+    private static final String CHANNEL_ID = "clinica_caduceo_channel";
+
+    // Variable de clase para almacenar el token
+    private static String fcmToken;
+
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // Envia el token al servidor para registrar el dispositivo
+        enviarTokenAlServidor(token);
+
+        // Almacena el token en las preferencias compartidas
+        almacenarTokenEnPreferencias(token);
+
+        // Almacena el token en la variable de clase
+        fcmToken = token;
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "Mensaje recibido de: " + remoteMessage.getFrom());
+
+        // Procesa el mensaje recibido
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Datos del mensaje: " + remoteMessage.getData());
+
+            // Extrae la fecha y hora del mensaje
+            String fechaHora = remoteMessage.getData().get("fecha_hora");
+            procesarFechaHora(fechaHora);
+
+            // Puedes manejar otros datos del mensaje aquí
+        }
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Cuerpo del mensaje: " + remoteMessage.getNotification().getBody());
+
+            // Crea una notificación local en la aplicación
+            mostrarNotificacion(remoteMessage.getNotification().getBody());
+        }
+    }
+
+    private void enviarTokenAlServidor(String token) {
+        // Lógica para enviar el token al servidor (puede ser una llamada HTTP)
+        // ...
+
+        // Ejemplo usando AsyncTask (ten en cuenta que AsyncTask no es la mejor opción, pero es simple para ilustrar)
+        new AsyncTask<String, Void, Void>() {
+            @Override
+            protected Void doInBackground(String... tokens) {
+                // Aquí deberías realizar la lógica para enviar el token al servidor
+                // tokens[0] contiene el token FCM
+                String token = tokens[0];
+                // ...
+
+                return null;
+            }
+        }.execute(token);
+    }
+
+    private void almacenarTokenEnPreferencias(String token) {
+        // Almacena el token en las preferencias compartidas
+        SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("fcm_token", token);
+        editor.apply();
+    }
+
+    private void procesarFechaHora(String fechaHora) {
+        // Formato de fecha y hora esperado: "yyyy-MM-dd HH:mm:ss"
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+
+        try {
+            Date fecha = formato.parse(fechaHora);
+            // Puedes trabajar con la fecha en tu lógica aquí
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void mostrarNotificacion(String cuerpoMensaje) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Clinica Caduceo Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle("Recordatorio de Cita")
+                .setContentText(cuerpoMensaje)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Notification notification = builder.build();
+        notificationManager.notify(0, notification);
+    }
+
+    // Función para obtener el token almacenado en la variable de clase
+    public static String getFcmToken() {
+        return fcmToken;
+    }
+}
+
